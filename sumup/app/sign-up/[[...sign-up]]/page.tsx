fba9bad2af0fa61e89e0a2b08e752852a40f69f8
muhirwJd <muhirwa232@gmail.com>
2025-04-18 23:03:26 +0200

    refactored sign up

--- sumup/app/sign-up/[[...sign-up]]/page.tsx
+++ sumup/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,33 +1,28 @@
+"use client";
+
 import Image from "next/image";
 import { SignUp } from "@clerk/nextjs";
+import { useSearchParams } from "next/navigation";
 
-type Props = {
-  searchParams?: { redirectUrl?: string };
-};
-
-export default function Register({ searchParams }: Props) {
-  const redirectUrl = searchParams?.redirectUrl || "/sign-in";
+export default function Register() {
+  const searchParams = useSearchParams();
+  const redirectUrl = searchParams.get("redirectUrl") || "/sign-in";
 
   return (
     <div className="flex h-screen bg-gradient-to-b from-white to-[#fff5cd]">
-      {/* Left Section - Register Form */}
       <div className="w-1/2 h-full flex items-center">
-        <div className="w-[479px] mx-auto">
-          <div className="space-y-5">
-            <SignUp
-              afterSignUpUrl={redirectUrl}
-              appearance={{
-                elements: {
-                  formButtonPrimary: "bg-[#E87C3E] hover:bg-[#d66d35]",
-                  footerActionLink: "text-[#E87C3E] hover:text-[#d66d35]",
-                },
-              }}
-            />
-          </div>
+        <div className="w-[479px] mx-auto space-y-5">
+          <SignUp
+            afterSignUpUrl={redirectUrl}
+            appearance={{
+              elements: {
+                formButtonPrimary: "bg-[#E87C3E] hover:bg-[#d66d35]",
+                footerActionLink: "text-[#E87C3E] hover:text-[#d66d35]",
+              },
+            }}
+          />
         </div>
       </div>
-
-      {/* Right Section - Card Payment Visual */}
       <div className="w-1/2 flex h-full justify-center items-center">
         <Image src="/Image.svg" width={500} height={800} alt="Card Payment" />
       </div>
